3fd7409ff6102e94d84310b8c02c3a2ec633839f
Alexander Micic <alexander.micic@gmail.com>
2017-06-06 23:06:03 +0200

    Add diff db

--- config/db.js
+++ config/db.js
@@ -2,9 +2,14 @@
 //   secret: "libapplication",
 //   url: "mongodb://localhost/library"
 // }
+// module.exports = {
+//   secret: "libapplication",
+//   url: "mongodb://alexm2017:node123@ds113282.mlab.com:13282/library-alexm2017"
+// }
 module.exports = {
   secret: "libapplication",
-  url: "mongodb://alexm2017:node123@ds113282.mlab.com:13282/library-alexm2017"
+  url: "mongodb://alexm2017:node123@cluster0-shard-00-00-cyn0u.mongodb.net:27017,cluster0-shard-00-01-cyn0u.mongodb.net:27017,cluster0-shard-00-02-cyn0u.mongodb.net:27017/library-angular2?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
+  
 }
 
 // Note: if the local connection fails then
